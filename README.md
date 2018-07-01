88743c53ae7ad264cd45a229498b5aca44d43547
InemGIBSON <inemgibs@gmail.com>
2018-07-01 23:16:50 +0100

    Final Commit

--- README.md
+++
@@ -1,9 +0,0 @@
-
-OFFLINE CURRENCY COVERTER
-# Designed and Implemented for the ALCwithGOOGLE3.0 challenge
-Author: GIBSON, Inemesit 
-Date: 1st July, 2018
-Country: Nigeria
-
-
-
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
+// Server to execute GIBS Converter APP on node.js
+// During Seven Days of Coding Challenge
+//#ALCwithGoogle3.0
 
-const express = require('express');
-const path = require('path');
-const app = express();
+const http = require('http');
+const fs = require('fs');
+function onRequest(request, response)
+{
+	response.writeHead(200, {'Content-Type':'text/html'});
+	fs.readFile('./index.html', null, function(error, data){
+		if(error){
+			response.writeHead(404);
+			response.write('File not found');
+		} else {
+			response.write(data);
+		}
+		response.end();
+	})
+}S
 
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-
-app.use(express.static(__dirname + '/public'));
-
-
-app.listen(8083, function() {
-  console.log('Server now on, App is running on Port 8083');
-});
-
-
-
+http.createServer(onRequest).listen(8082);
